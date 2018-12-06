6ea1b740de596211be45b9af30032b54cfcc1d1e
dat <dat@elementaryrobotics.com>
2018-12-06 14:44:37 -0800

    Logging can be given a time range and filter on certain elements. Sending commands is also implemented.

--- tools/cli/atom-cli.py
+++ tools/cli/atom-cli.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import json
+import shlex
 from atom import Element
 from prompt_toolkit import prompt, HTML, PromptSession
 from prompt_toolkit import print_formatted_text as print
@@ -17,14 +19,16 @@ class AtomCLI:
             "help": self.cmd_help,
             "list": self.cmd_list,
             "logs": self.cmd_logs,
+            "command": self.cmd_command,
         }
         self.command_completer = WordCompleter(self.cmd_map.keys())
+        self.indent = 2
 
     def run(self):
         while True:
-            inp = self.session.prompt("\n> ",
-                auto_suggest=AutoSuggestFromHistory(),
-                completer=self.command_completer).split()
+            inp = shlex.split(self.session.prompt(
+                "\n> ", auto_suggest=AutoSuggestFromHistory(),
+                completer=self.command_completer))
             if not inp:
                 continue
             command, args = inp[0], inp[1:]
@@ -69,14 +73,25 @@ class AtomCLI:
             print(item)
 
     def cmd_logs(self, *args):
-        if not args:
-            print("'logs' must have an argument.")
-            return
-        element_name = args[0]
-        log_id = self.element._make_log_id(element_name)
-        logs = self.element._rclient.xrange(log_id)
+        if args and args[0].isdigit():
+            ms = int(args[0]) * 1000
+            start_time = str(int(self.element._get_redis_timestamp()) - ms)
+            elements = set(args[1:])
+        else:
+            start_time = "-"
+            elements = set(args)
+
+        if elements:
+            all_logs = self.element._rclient.xrange("log", start=start_time)
+            logs = []
+            for timestamp, content in all_logs:
+                if content[b"element"].decode() in elements:
+                    logs.append((timestamp, content))
+        else:
+            logs = self.element._rclient.xrange("log", start=start_time)
+
         if not logs:
-            print(f"No logs for {element_name}.")
+            print("No logs.")
             return
         for log in self.format_logs(logs):
             print(log)
@@ -84,24 +99,54 @@ class AtomCLI:
     def format_logs(self, logs):
         formatted_logs = []
         for timestamp, content in logs:
-            formatted_log = {
+            formatted_log = json.dumps({
+                "element": content[b"element"].decode(),
                 "timestamp": timestamp.decode(),
                 "level": content[b"level"].decode(),
                 "msg": content[b"msg"].decode(),
-            }
+            }, indent=self.indent)
             formatted_logs.append(formatted_log)
         return formatted_logs
+
+    def cmd_command(self, *args):
+        if len(args) < 2:
+            print("Too few arguments.")
+            return
+        if len(args) > 3:
+            print("Too many arguments.")
+            return
+        element_name = args[0]
+        command_name = args[1]
+        if len(args) == 3:
+            data = args[2]
+        else:
+            data = ""
+        resp = self.element.command_send(element_name, command_name, data)
+        formatted_resp = json.dumps({
+            "err_code": resp["err_code"],
+            "err_str": resp["err_str"],
+            "data": repr(resp["data"]),
+        }, indent=self.indent)
+        print(formatted_resp)
 
 
 if __name__ == "__main__":
     # TODO REMOVE TESTING STUFF
+    import threading
+    from atom.messages import Response
+    def add_1(x):
+        return Response(int(x)+1)
+
     elem1 = Element("element1")
-    elem1.log(7, "element1 log 1", stdout=False)
-    elem1.log(7, "element1 log 2", stdout=False)
+    elem1.command_add("add_1", add_1)
+    t = threading.Thread(target=elem1.command_loop, daemon=True)
+    t.start()
+    # elem1.log(7, "element1 log 1", stdout=False)
+    # elem1.log(7, "element1 log 2", stdout=False)
 
     elem2 = Element("element2")
-    elem2.log(7, "element2 log 1", stdout=False)
-    elem2.log(7, "element2 log 2", stdout=False)
+    # elem2.log(7, "element2 log 1", stdout=False)
+    # elem2.log(7, "element2 log 2", stdout=False)
     ############################
 
     atom_cli = AtomCLI()
