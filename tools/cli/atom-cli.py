994f01d84908a2f35b8a7853988c866933652e2e
dat <dat@elementaryrobotics.com>
2018-12-07 17:06:04 -0800

    Create records command and create function for formatting entries in redis

--- tools/cli/atom-cli.py
+++ tools/cli/atom-cli.py
@@ -29,7 +29,7 @@ class AtomCLI:
         self.cmd_map = {
             "help": self.cmd_help,
             "list": self.cmd_list,
-            "logs": self.cmd_logs,
+            "records": self.cmd_records,
             "command": self.cmd_command,
             "read": self.cmd_read,
             "exit": self.cmd_exit,
@@ -90,42 +90,76 @@ class AtomCLI:
         for item in items:
             print(item)
 
-    def cmd_logs(self, *args):
-        if args and args[0].isdigit():
-            ms = int(args[0]) * 1000
-            start_time = str(int(self.element._get_redis_timestamp()) - ms)
-            elements = set(args[1:])
-        else:
-            start_time = "-"
-            elements = set(args)
-
-        if elements:
-            all_logs = self.element._rclient.xrange("log", start=start_time)
-            logs = []
-            for timestamp, content in all_logs:
-                if content[b"element"].decode() in elements:
-                    logs.append((timestamp, content))
-        else:
-            logs = self.element._rclient.xrange("log", start=start_time)
-
-        if not logs:
-            print("No logs.")
+    def cmd_records(self, *args):
+        if not args:
+            print("'records' must have an argument.")
             return
-        for log in self.format_logs(logs):
-            print(log)
+        mode = args[0]
 
-    def format_logs(self, logs):
-        formatted_logs = []
-        for timestamp, content in logs:
-            formatted_log = json.dumps({
-                "element": content[b"element"].decode(),
-                "timestamp": timestamp.decode(),
-                "level": content[b"level"].decode(),
-                "msg": content[b"msg"].decode(),
-            }, indent=self.indent)
-            formatted_logs.append(formatted_log)
-        return formatted_logs
+        # Check for start time
+        if len(args) > 1 and args[1].isdigit():
+            ms = int(args[1]) * 1000
+            start_time = str(int(self.element._get_redis_timestamp()) - ms)
+            elements = set(args[2:])
+        else:
+            # If no start time, go from the very beginning
+            start_time = "-"
+            elements = set(args[1:])
 
+        if mode == "log":
+            records = self.mode_log(start_time, elements)
+        elif mode == "cmdres":
+            if not elements:
+                print("Must provide elements from which to get command response streams from.")
+                return
+            records = self.mode_cmdres(start_time, elements)
+        else:
+            print("Invalid argument to 'records'.")
+            return
+
+        if not records:
+            print("No records.")
+            return
+        for record in records:
+            print(self.format_record(record))
+
+    def mode_log(self, start_time, elements):
+        records = []
+        all_records = self.element._rclient.xrange("log", start=start_time)
+        for timestamp, content in all_records:
+            if not elements or content[b"element"].decode() in elements:
+                content["timestamp"] = timestamp.decode()
+                records.append(content)
+        return records
+
+    def mode_cmdres(self, start_time, elements):
+        streams, records = [], []
+        for element in elements:
+            streams.append(self.element._make_response_id(element))
+            streams.append(self.element._make_command_id(element))
+        for stream in streams:
+            cur_records = self.element._rclient.xrange(stream, start=start_time)
+            for timestamp, content in cur_records:
+                content["type"], content["element"] = stream.split(":")
+                content["timestamp"] = timestamp.decode()
+                records.append((timestamp.decode(), content))
+        # Sort records by timestamp
+        # If the timestamps are the same, put commands before responses
+        return [record[1] for record in sorted(records, key=lambda x: (x[0], x[1]["type"]))]
+
+    def format_record(self, record):
+        formatted_record = {}
+        for k, v in record.items():
+            if type(k) is bytes:
+                k = k.decode()
+            try:
+                v = v.decode()
+            except:
+                v = str(v)
+            formatted_record[k] = v
+        sorted_record = {k:v for k, v in sorted(formatted_record.items(), key=lambda x: x[0])}
+        return json.dumps(sorted_record, indent=self.indent)
+            
     def cmd_command(self, *args):
         if len(args) < 2:
             print("Too few arguments.")
@@ -140,12 +174,7 @@ class AtomCLI:
         else:
             data = ""
         resp = self.element.command_send(element_name, command_name, data)
-        formatted_resp = json.dumps({
-            "err_code": resp["err_code"],
-            "err_str": resp["err_str"],
-            "data": repr(resp["data"]),
-        }, indent=self.indent)
-        print(formatted_resp)
+        print(self.format_record(resp))
 
     def cmd_read(self, *args):
         if len(args) < 1:
@@ -157,11 +186,11 @@ class AtomCLI:
         element_name, stream_name = args[0].split(":")
         if len(args) == 2:
             try:
-                rate = int(args[1])
-                if rate < 1:
+                rate = float(args[1])
+                if rate < 0:
                     raise ValueError()
             except ValueError:
-                print("rate must be an integer greater than 1.")
+                print("rate must be an float greater than 0.")
                 return
         else:
             rate = None
@@ -177,13 +206,7 @@ class AtomCLI:
             timestamp = entry["timestamp"]
             if timestamp != last_timestamp:
                 last_timestamp = timestamp
-                entry_formatted = {"timestamp": entry["timestamp"]}
-                for k, v in entry.items():
-                    if type(v) is bytes:
-                        entry_formatted[k] = str(v)
-                    else:
-                        entry_formatted[k] = v
-                print(json.dumps(entry_formatted, indent=self.indent))
+                print(self.format_record(entry))
             if rate:
                 time.sleep(max(1 / rate - (time.time() - start_time), 0))
 
@@ -197,12 +220,12 @@ if __name__ == "__main__":
     # TODO REMOVE TESTING STUFF
     import threading
     from atom.messages import Response
-    # def add_1(x):
-        # return Response(int(x)+1)
+    def add_1(x):
+        return Response(int(x)+1)
 
 
     elem1 = Element("element1")
-    # elem1.command_add("add_1", add_1)
+    elem1.command_add("add_1", add_1)
 
     def loop():
         i = 0
@@ -213,6 +236,8 @@ if __name__ == "__main__":
 
     t = threading.Thread(target=loop, daemon=True)
     t.start()
+    t1 = threading.Thread(target=elem1.command_loop, daemon=True)
+    t1.start()
     # elem1.log(7, "element1 log 1", stdout=False)
     # elem1.log(7, "element1 log 2", stdout=False)
 
