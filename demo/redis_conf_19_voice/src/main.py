254fbc1004d90f429bd277565c4689746775debb
nambi-srivatsav <vatsa9myself@gmail.com>
2019-04-02 13:58:41 -0700

    redis_conf_19_voice:blocking call for voice
    1. Added blocking call for the latest entry on voice.
    2. Cleaned and formatted the code.

--- demo/redis_conf_19_voice/src/main.py
+++ demo/redis_conf_19_voice/src/main.py
@@ -5,44 +5,76 @@ from threading import Thread
 import pdb
 import time
 
+PUBLISH_FREQUENCY = 100
+TIME_FOR_WAVEFORM = 5
+
 if __name__ == "__main__":
 
     element = Element("voice_wave")
-    ## Write entries to the same stream to clear
-    voice_element = Element("voice")
-    voice_element.entry_write("string", {"data" : "clear"})
-    
-    ## Start recording.
-    data = {"name":"example", "t":5, "perm":False, "e":"waveform", "s":"serialized"}
+
+    data = {
+        "name": "example",
+        "t": TIME_FOR_WAVEFORM,
+        "perm": False,
+        "e": "waveform",
+        "s": "serialized"
+    }
     res = element.command_send("record", "start", data, serialize=True)
-    time.sleep(7)
-    
-    sinx_strings = ["sinex","sinx"]
+    time.sleep(TIME_FOR_WAVEFORM + 2)
+
+    sinx_strings = ["sinex", "sinx"]
     cosx_strings = ["cosx"]
-    tanx_strings = ["tanx","10x","tonics"]
+    tanx_strings = ["tanx", "10x", "tonics"]
 
     while True:
-        ### get voice_string
         print("listening..")
-        entry = element.entry_read_n("voice", "string", 1)
-        voice_string = entry[0]['data'].decode()
+        last_id = element._get_redis_timestamp()
 
-        if any(x in voice_string for x in sinx_strings):
-            print("Plotting sinx")
-            data  = { "name":"example", "msgpack":True, "plots":[ { "data": [[ "x", ["sin"], "value" ]] } ] }
-            res = element.command_send("record", "plot", data, serialize=True)
-            voice_element.entry_write("string", {"data" : "clear"})
+        while True:
+            entries = element.entry_read_since(
+                "voice", "string", last_id=last_id, block=1000)
+            if entries:
+                last_id = entries[0]["id"]
+                #voice_string = entries[0]["data"].decode().strip().lower().replace("-", "").split(" ")
+                voice_string = entries[0]["data"].decode()
 
-        if any(x in voice_string for x in cosx_strings):
-            print("Plotting cosx")
-            data  = { "name":"example", "msgpack":True, "plots":[ { "data": [[ "x", ["cos"], "value" ]] } ] }
-            res = element.command_send("record", "plot", data, serialize=True)
-            voice_element.entry_write("string", {"data" : "clear"})
-        
-        if any(x in voice_string for x in tanx_strings):
-            print("Plotting tanx")
-            data  = { "name":"example", "msgpack":True, "plots":[ { "data": [[ "x", ["tan"], "value" ]] } ] }
-            res = element.command_send("record", "plot", data, serialize=True)
-            voice_element.entry_write("string", {"data" : "clear"})
+                if any(x in voice_string for x in sinx_strings):
 
-        time.sleep(0.01)
+                    print("Plotting sinx")
+                    data = {
+                        "name": "example",
+                        "msgpack": True,
+                        "plots": [{
+                            "data": [["x", ["sin"], "value"]]
+                        }]
+                    }
+                    res = element.command_send(
+                        "record", "plot", data, serialize=True)
+
+                if any(x in voice_string for x in cosx_strings):
+
+                    print("Plotting cosx")
+                    data = {
+                        "name": "example",
+                        "msgpack": True,
+                        "plots": [{
+                            "data": [["x", ["cos"], "value"]]
+                        }]
+                    }
+
+                    res = element.command_send(
+                        "record", "plot", data, serialize=True)
+
+                if any(x in voice_string for x in tanx_strings):
+                    print("Plotting tanx")
+                    data = {
+                        "name": "example",
+                        "msgpack": True,
+                        "plots": [{
+                            "data": [["x", ["tan"], "value"]]
+                        }]
+                    }
+                    res = element.command_send(
+                        "record", "plot", data, serialize=True)
+
+                time.sleep(1 / PUBLISH_FREQUENCY)
