7106893ed84b9dfffea7f14f418d7fc8fa06de4d
Dan Pipe-Mazo <dpipemazo@gmail.com>
2020-05-16 15:09:29 -0700

    minimize: working

--- base/build_base.sh
+++ base/build_base.sh
@@ -8,6 +8,10 @@ RUN_BUILD="y"
 REGISTRY_COMPOSE=docker-compose-registry.yml
 REGISTRY_LOCATION=localhost:5000
 
+#
+# Set up
+#
+
 # Launch the registry
 docker-compose -f ${REGISTRY_COMPOSE} up -d
 
@@ -21,6 +25,10 @@ docker run --rm --privileged docker/binfmt:66f9012c56a8316f9244ffd7622d7c21c1f6f
 #   to the registry
 docker buildx create --use --name basebuilder --driver-opt network=host
 docker buildx inspect --bootstrap
+
+#
+# Build Dockerfiles
+#
 
 # Note that we're starting with the original base image
 CURRENT_BASE=${4}
@@ -65,6 +73,50 @@ do
     # Move the current base
     CURRENT_BASE=${NEW_IMAGE}
 done
+
+#
+# Minimize
+#
+
+# Now, we want to minimize
+ARGS_FILE=${5}/minimize-${1}-args
+ADDITIONAL_ARGS=""
+if [ -f ${ARGS_FILE} ]; then
+    while read arg; do
+        ADDITIONAL_ARGS+="--build-arg ${arg}"
+    done < ${ARGS_FILE}
+fi
+
+# Do the build
+NEW_IMAGE=${REGISTRY_LOCATION}/${2}:base-minimize-${1}
+CMD_STRING="docker buildx build  \
+    -f utilities/Dockerfile-minimize \
+    --platform=linux/${1}  \
+    -t ${NEW_IMAGE}  \
+    --progress=plain  \
+    --push \
+    --build-arg BASE_IMAGE=${CURRENT_BASE} \
+    --build-arg PRODUCTION_IMAGE=${4}
+    --pull=false \
+    ${ADDITIONAL_ARGS} \
+    ../."
+echo ${CMD_STRING}
+if [ ${RUN_BUILD} == "y" ]; then
+    ${CMD_STRING}
+fi
+
+# Check the exit status
+if [ $? != "0" ]; then
+    echo "Build of minimized Dockerfile failed! Exiting"
+    exit 1
+fi
+
+# Note the new base
+CURRENT_BASE=${NEW_IMAGE}
+
+#
+# Package it up
+#
 
 # Note the final output tag
 OUTPUT_TAG="${2}:${3}-${5}-${1}"
