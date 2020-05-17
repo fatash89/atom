abb3fe2fbbd86ceff168eb879d9679feac1cb98a
Dan Pipe-Mazo <dpipemazo@gmail.com>
2020-05-17 07:27:12 -0700

    graphics: able to package PyQt5

--- base/utilities/ldd_find_libs.sh
+++ base/utilities/ldd_find_libs.sh
@@ -5,18 +5,19 @@
 #   that we can copy them over
 #
 
-DEFAULT_LIB_LOCATIONS="/usr/local/lib/*.so* /usr/local/lib/*.a* /usr/local/bin"
+DEFAULT_LIB_LOCATIONS="/usr/local/lib/*.so* /usr/local/lib/*.a* /usr/local/bin/*"
 
 LDD_CMD=""
-for location in DEFAULT_LIB_LOCATIONS; do
-    LDD_CMD+=$(ls ${location})
+for location in ${DEFAULT_LIB_LOCATIONS}; do
+    LDD_CMD+="$(ls --format=commas ${location}) "
 done
 
-for location in ADDITIONAL_LIB_LOCATIONS; do
-    LDD_CMD+=$(ls ${location})
+for location in ${ADDITIONAL_LIB_LOCATIONS}; do
+    LDD_CMD+="$(ls --format=commas ${location}) "
 done
 
 # Run the command
+LDD_CMD=$(echo ${LDD_CMD} | sed "s/,/ /g" )
 echo "About to run LDD on ${LDD_CMD}"
 ldd ${LDD_CMD} | grep "=> /" | awk '{print $3}' | sort -u > /tmp/required_libs.txt
 
