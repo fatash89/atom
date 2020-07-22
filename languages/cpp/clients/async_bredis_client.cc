da359ed867d2bc1bce86140991a7b19b61f49924
BarbBoyaji <boyaji99@gmail.com>
2020-07-22 10:43:34 -0700

    fix conn in async bredis client

--- languages/cpp/clients/async_bredis_client.cc
+++ languages/cpp/clients/async_bredis_client.cc
@@ -3,6 +3,7 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <csignal>
 
 #include <bredis/Connection.hpp>
 #include <bredis/Extract.hpp>
@@ -14,51 +15,82 @@ template<typename socket, typename endpoint, typename buffer, typename iterator,
 class bredis_client {
 
     public:
-    bredis_client(boost::asio::io_context & ioserv, std::string ip_address, int port, std::string & img_str) : ioserv(ioserv),
-                ep(boost::asio::ip::address::from_string(ip_address), boost::lexical_cast<std::uint16_t>(port)), sock(ioserv), bredis_con(std::move(sock)) {
+    //constructor for tcp socket connection
+    bredis_client(boost::asio::io_context & iocon, std::string ip_address, int port, std::string & img_str) : 
+        iocon(iocon),
+        ep(boost::asio::ip::address::from_string(ip_address), 
+        boost::lexical_cast<std::uint16_t>(port)), 
+        sock(std::make_shared<socket>(iocon)), 
+        bredis_con(nullptr) {
+        img = img_str;
+        counter = 0;
+    }
+    
+    //constructor for unix socket connection
+    bredis_client(boost::asio::io_context & iocon, std::string sock_addr, std::string & img_str) : 
+            iocon(iocon),
+            ep(sock_addr), 
+            sock(std::make_shared<socket>(iocon)), 
+            bredis_con(nullptr) {
         img = img_str;
         counter = 0;
     }
 
-    ~bredis_client(){}
+    ~bredis_client(){
+        std::cout<<"Destruction."<<std::endl;
+        if(sock->is_open()){
+            sock->close();
+        }
+    }
+    
+    void stop(){
+        std::cout<< "Closing Socket" <<std::endl;
+        sock->close();
+    }
 
     void start(){
-        sock.async_connect(ep, std::bind(&bredis_client::on_connect, this, std::placeholders::_1));
+        sock->async_connect(ep, std::bind(&bredis_client::on_connect, this, std::placeholders::_1));
     }
 
     void on_connect(const boost::system::error_code& err){
-        if(!err){
+        if(err){
+            std::cout<<"Connection was unsuccessful: "<<  err.message() << std::endl;   
+            stop();        
+        } else{
             std::cout<<"Connection to Redis was successful: " << err.message() <<std::endl;
-            if(!sock.is_open()){
+            if(!sock->is_open()){
                 std::cout<<"Socket closed... Connection timed out." <<std::endl;
             } else{
+                bredis_con = std::make_shared<bredis::Connection<socket>>(std::move(*sock));
                 do_async_xadd();
             }
-        } else{
-            std::cout<<"Connection was unsuccessful: "<<  err.message() << std::endl;
         }
     }
 
     void do_async_xadd(){
         std::cout<<"do_async_xadd"<<std::endl;
-        bredis_con.async_write(xadd_tx_buff, bredis::single_command_t{ "XADD", "bredis", "*",  "nasa_pic", img}, std::bind(&bredis_client::on_async_xadd_write, this, std::placeholders::_1, std::placeholders::_2 ));
+        bredis_con->async_write(xadd_tx_buff, bredis::single_command_t{ "XADD", "bredis", "*",  "nasa_pic", "picture"}, std::bind(&bredis_client::on_async_xadd_write, this, std::placeholders::_1, std::placeholders::_2 ));
     }
 
     void on_async_xadd_write(const boost::system::error_code &err, std::size_t bytes_transferred){
         std::cout<<"on_async_xadd_write"<<std::endl;
-        if(!err){
+        if(err){
+            std::cout<<"XADD write was unsuccessful: "<<  err.message() << std::endl;
+            stop();         
+        } else{
             // tx_buff must be consumed when it is no longer needed
             xadd_tx_buff.consume(bytes_transferred);
-            bredis_con.async_read(xadd_rx_buff, std::bind(&bredis_client::on_xadd_read, this, std::placeholders::_1, std::placeholders::_2));
-        } else{
-            std::cout<<"XADD write was unsuccessful: "<<  err.message() << std::endl;
+            bredis_con->async_read(xadd_rx_buff, std::bind(&bredis_client::on_xadd_read, this, std::placeholders::_1, std::placeholders::_2));
         }
     }
 
     void on_xadd_read(const boost::system::error_code &err, bredis::parse_result_mapper_t<iterator, policy> &&result_markers) {
         std::cout<<"on_xadd_read"<<std::endl;
 
-        if(!err){                
+        if(err){                
+            std::cout<<"XADD read was unsuccessful: "<<  err.message() << std::endl;
+            stop();
+        } else{
             // consume the buffers, after finishing work with the markers
             xadd_rx_buff.consume(result_markers.consumed);
 
@@ -66,19 +98,16 @@ class bredis_client {
                 std::cout<<"continuing..."<<std::endl;
                 do_async_xadd();
             } else{
-                std::cout<<"Closing socket."<<std::endl;
-                sock.close();
-            }
-        } else{
-            std::cout<<"XADD read was unsuccessful: "<<  err.message() << std::endl;
+                stop();
+            }        
         }
     }
 
     //members
-    boost::asio::io_context& ioserv;
+    boost::asio::io_context& iocon;
     endpoint ep;
     std::shared_ptr<socket> sock;
-    bredis::Connection<socket> bredis_con;
+    std::shared_ptr<bredis::Connection<socket>> bredis_con;
     int counter;
     buffer xadd_tx_buff;
     buffer xadd_rx_buff;
@@ -87,6 +116,7 @@ class bredis_client {
 
 
 int main(){
+
     // log file setup here
     std::ofstream bredis_logfile;
     bredis_logfile.open("async_bredis.log", std::ios_base::app);
@@ -106,24 +136,21 @@ int main(){
 
     int iters = 100;
 
-    //defining types here
-    using socket_t = boost::asio::ip::tcp::socket;
+    /* tcp socket types */
+    /* using socket_t = boost::asio::ip::tcp::socket;
     using endpoint_t = boost::asio::ip::tcp::endpoint;
+    using next_layer_t = socket_t; */
+    
+    /* unix socket types */
+    using socket_t = boost::asio::local::stream_protocol::socket;
     using next_layer_t = socket_t;
+    using endpoint_t = boost::asio::local::stream_protocol::endpoint;
 
     using Buffer = boost::asio::streambuf;
     using Iterator = typename bredis::to_iterator<Buffer>::iterator_t;
     using Policy = bredis::parsing_policy::keep_result;
         
-    boost::asio::io_context ioserv;
-
-    /* unix socket here */
-    //using socket_t = boost::asio::local::stream_protocol::socket;
-    //using next_layer_t = socket_t;
-
-    //boost::asio::local::stream_protocol::endpoint end_point("/shared/redis.sock");
-    //socket_t sock(ioserv);
-    //sock.connect("/shared/redis.sock");
+    boost::asio::io_context iocon;
 
     //read image in
     std::ifstream img_file("./clients/data/nasa1.jpg", std::ios::binary);
@@ -133,7 +160,8 @@ int main(){
     std::chrono::time_point<std::chrono::high_resolution_clock> start_w = std::chrono::high_resolution_clock::now();
 
 
-    bredis_client<socket_t, endpoint_t, Buffer, Iterator, Policy> bredis_cli(ioserv, "172.19.0.2", 6379, img_string);
+    //bredis_client<socket_t, endpoint_t, Buffer, Iterator, Policy> bredis_cli(iocon, "172.19.0.2", 6379, img_string);
+    bredis_client<socket_t, endpoint_t, Buffer, Iterator, Policy> bredis_cli(iocon, "/shared/redis.sock", img_string);
 
     bredis_cli.start();
 
@@ -141,7 +169,15 @@ int main(){
     std::chrono::time_point<std::chrono::high_resolution_clock> stop_w = std::chrono::high_resolution_clock::now();
     auto dur_w = std::chrono::duration_cast<std::chrono::milliseconds>(stop_w - start_w);
 
+    //signal handling
+/*     boost::asio::signal_set signals(iocon, SIGINT);
+    signals.async_wait([&](const boost::system::error_code& error, int signal_number) {
+                                bredis_cli.stop();
+                                return signal_number;
+                            }); */
 
-    ioserv.run();
+    iocon.run();
+
+    return 0;
 
 }
