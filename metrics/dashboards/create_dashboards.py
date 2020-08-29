eb221a88efd6e6904f5be576a8f6a6a345398b87
Dan Pipe-Mazo <dpipemazo@gmail.com>
2020-08-29 13:26:01 -0700

    create_dashboards.py: Format with black

--- metrics/dashboards/create_dashboards.py
+++ metrics/dashboards/create_dashboards.py
@@ -20,7 +20,7 @@ import requests
 import os
 import redis
 import time
-from jinja2 import FileSystemLoader, Environment, PackageLoader, select_autoescape
+from jinja2 import FileSystemLoader, Environment, select_autoescape
 
 GRAFANA_USER = os.getenv("GRAFANA_USER", "admin")
 GRAFANA_PASSWORD = os.getenv("GRAFANA_PASSWORD", "admin")
@@ -29,7 +29,7 @@ GRAFANA_URL = f"http://{GRAFANA_USER}:{GRAFANA_PASSWORD}@{os.getenv('GRAFANA_URL
 # Info to make all of the dashboards
 METRICS_TIMING = [
     # Data bucketed by 10m, default show last hour
-    (600000,  "1h"),
+    (600000, "1h"),
     # Data bucketed by 1h, default show last 24 hours
     (3600000, "24h"),
     # Data bucketed by 24h, default show last 7 days
@@ -41,35 +41,32 @@ ATOM_SERVER_PORT = int(os.getenv("ATOM_SERVER_PORT", "6379"))
 METRICS_SERVER_IP = os.getenv("METRICS_SERVER_IP", "localhost")
 METRICS_SERVER_PORT = int(os.getenv("METRICS_SERVER_PORT", "6380"))
 DATASOURCES = [
-    ("Atom Nucleus", "redis-atom",      f"{ATOM_SERVER_IP}:{ATOM_SERVER_PORT}"),
-    ("Atom Metrics", "redis-metrics",   f"{METRICS_SERVER_IP}:{METRICS_SERVER_PORT}")
+    ("Atom Nucleus", "redis-atom", f"{ATOM_SERVER_IP}:{ATOM_SERVER_PORT}"),
+    ("Atom Metrics", "redis-metrics", f"{METRICS_SERVER_IP}:{METRICS_SERVER_PORT}"),
 ]
 
 # List of system-level dashboards we want to make at boot
-SYS_DASHBOARDS = [ "cpu", "disk", "memory", "network" ]
+SYS_DASHBOARDS = ["cpu", "disk", "memory", "network"]
 
 # Wait for grafana to come up
 print("Waiting for Grafana to come up...")
 while True:
     try:
-        data = requests.get(GRAFANA_URL + '/api/health')
+        data = requests.get(GRAFANA_URL + "/api/health")
         if data.ok:
             break
-    except:
+    except:  # noqa: E722
         print("Grafana server not yet up, waiting")
         time.sleep(1)
 print("Grafana up!")
 
 # Get the template
 templateLoader = FileSystemLoader(searchpath="./templates")
-env = Environment(
-    loader=templateLoader,
-    autoescape=select_autoescape(['html', 'xml'])
-)
+env = Environment(loader=templateLoader, autoescape=select_autoescape(["html", "xml"]))
 
 # Create the datasources
 print("Creating datasources...")
-datasource_template = env.get_template('datasource.json.j2')
+datasource_template = env.get_template("datasource.json.j2")
 for source in DATASOURCES:
     data = requests.post(
         GRAFANA_URL + "/api/datasources",
@@ -78,33 +75,35 @@ for source in DATASOURCES:
                 name=source[1],
                 redis_ip=source[2],
             )
-        )
+        ),
     )
     if not data.ok:
-        print(f"Failed to create datasource {source}: status: {data.status_code} response: {data.json()}")
+        print(
+            f"Failed to create datasource {source}: status: {data.status_code} response: {data.json()}"
+        )
 
 # Create the dashboards for the redises themselves
 print("Creating Redis Overview Dashboards...")
-overview_template = env.get_template('redis_overview.json.j2')
+overview_template = env.get_template("redis_overview.json.j2")
 for source in DATASOURCES:
     data = requests.post(
         GRAFANA_URL + "/api/dashboards/db",
-        json=json.loads(
-            overview_template.render(
-                name=source[0],
-                datasource=source[1]
-            )
-        )
+        json=json.loads(overview_template.render(name=source[0], datasource=source[1])),
     )
     if not data.ok:
-        print(f"Failed to create overview dashboard for source {source}: status: {data.status_code} response: {data.json()}")
+        print(
+            f"Failed to create overview dashboard for source {source}: "
+            f"status: {data.status_code} response: {data.json()}"
+        )
 
 # Create system overview dashboards
 print("Creating System Overview Dashboards...")
 for sys_dash in SYS_DASHBOARDS:
-    template = env.get_template(f'{sys_dash}.json.j2')
+    template = env.get_template(f"{sys_dash}.json.j2")
 
-    for timing in [(None, None),] + METRICS_TIMING:
+    for timing in [
+        (None, None),
+    ] + METRICS_TIMING:
         if timing[0] is not None:
             bucket = timing[0]
             agg_label = f"{timing[0] // (1000 * 60)}m"
@@ -124,12 +123,15 @@ for sys_dash in SYS_DASHBOARDS:
                     bucket=bucket,
                     agg_label=agg_label,
                     datasource="redis-metrics",
-                    start_time=start_time
+                    start_time=start_time,
                 )
-            )
+            ),
         )
         if not data.ok:
-            print(f"Failed to create overview dashboard for source {source}: status: {data.status_code} response: {data.json()}")
+            print(
+                f"Failed to create overview dashboard for source {source}: "
+                f"status: {data.status_code} response: {data.json()}"
+            )
 
 
 # Figure out how many elements we have that support metrics
@@ -138,15 +140,17 @@ redis_client = redis.StrictRedis(host=METRICS_SERVER_IP, port=METRICS_SERVER_POR
 metrics_keys = redis_client.keys()
 metrics_elements = set()
 for key in metrics_keys:
-    element = key.decode('utf-8').split(":")[0]
+    element = key.decode("utf-8").split(":")[0]
     if element not in metrics_elements:
         metrics_elements.add(element)
 
 # Create the dashboards for the elements
 print(f"Setting up dashboards for elements: {metrics_elements}")
-element_template = env.get_template('default_element.json.j2')
+element_template = env.get_template("default_element.json.j2")
 for element in metrics_elements:
-    for timing in [(None, None),] + METRICS_TIMING:
+    for timing in [
+        (None, None),
+    ] + METRICS_TIMING:
         if timing[0] is not None:
             bucket = timing[0]
             agg_label = f"{timing[0] // (1000 * 60)}m"
@@ -167,35 +171,34 @@ for element in metrics_elements:
                     agg_label=agg_label,
                     name=name,
                     datasource="redis-metrics",
-                    start_time=start_time
+                    start_time=start_time,
                 )
-            )
+            ),
         )
         if not data.ok:
-            print(f"Failed to create dashboard: status: {data.status_code} response: {data.json()}")
+            print(
+                f"Failed to create dashboard: status: {data.status_code} response: {data.json()}"
+            )
 
 # Create the home dashboard
 print("Creating/Updating Home Dashboard...")
-home_template = env.get_template('home.json.j2')
+home_template = env.get_template("home.json.j2")
 data = requests.post(
     GRAFANA_URL + "/api/dashboards/db",
-    json=json.loads(
-        home_template.render(
-            datasource="redis-metrics"
-        )
-    )
+    json=json.loads(home_template.render(datasource="redis-metrics")),
 )
 if not data.ok:
-    print(f"Failed to create home dashboard: status: {data.status_code} response: {data.json()}")
+    print(
+        f"Failed to create home dashboard: status: {data.status_code} response: {data.json()}"
+    )
 
 # Now we want to set the home dashboard as the default
 data = requests.put(
-    GRAFANA_URL + "/api/org/preferences",
-    json={
-        "homeDashboardId": data.json()["id"]
-    }
+    GRAFANA_URL + "/api/org/preferences", json={"homeDashboardId": data.json()["id"]}
 )
 if not data.ok:
-    print(f"Failed to update home dashboard: status: {data.status_code} response: {data.json()}")
+    print(
+        f"Failed to update home dashboard: status: {data.status_code} response: {data.json()}"
+    )
 
 print("Dashboard setup complete!")
