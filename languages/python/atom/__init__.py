855ebc2c50232c59eb9d93fc148d24f0a61a3d4c
Dan Pipe-Mazo <dpipemazo@gmail.com>
2021-01-26 14:07:04 -0800

    __init__: include LogLevel and MetricsLevel

--- languages/python/atom/__init__.py
+++ languages/python/atom/__init__.py
@@ -1,4 +1,5 @@
-from atom.element import Element, AtomError, SetEmptyError, MetricsLevel  # noqa F401
-from atom.config import VERSION
+from atom.element import Element, AtomError, SetEmptyError  # noqa F401
+from atom.config import VERSION, MetricsLevel
+from atom.messages import LogLevel
 
 __version__ = VERSION
