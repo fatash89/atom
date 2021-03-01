f6993da97231c9d22c4d8d1f0f418f9df56117fb
Kyle Bebak <kylebebak@gmail.com>
2021-03-01 13:40:59 -0600

    Split work to publish Atom PyPI library out into separate PR

--- languages/python/setup.py
+++ languages/python/setup.py
@@ -10,14 +10,6 @@ except Exception:
 setuptools.setup(
     name="atom",
     packages=["atom"],
-    description="Python bindings for the Elementary Robotics Atom SDK",
-    url="https://github.com/elementary-robotics/atom",
-    author="Elementary Robotics",
-    install_requires=[
-        "msgpack==0.6.2",
-        "redis@git+https://github.com/andymccurdy/redis-py.git#2c9f41f46991f94f0626598600d1023d4e12f0bc",
-        "redistimeseries@git+https://github.com/RedisTimeSeries/redistimeseries-py.git#b451b43c564076acfe8de62eb001277876631a9a",  # noqa: E501
-    ],
     version=__version__,
     include_package_data=True,
 )
