1d31311dc8cc8103c574901189ce614056e41c25
Kyle Bebak <kylebebak@gmail.com>
2021-03-10 16:46:07 -0600

    Preparing to publish on PyPI

--- languages/python/setup.py
+++ languages/python/setup.py
@@ -1,15 +1,34 @@
 import setuptools
-import version
-
-try:
-    from atom import __version__
-except Exception:
-    __version__ = version.main()
 
 
 setuptools.setup(
-    name="atom",
+    name="elementary-atom",
     packages=["atom"],
-    version=__version__,
+    version="0.0.1",
     include_package_data=True,
+    keywords="elementary robotics atom redis rts",
+    url="https://github.com/elementary-robotics/atom",
+    author="Elementary Robotics",
+    author_email="dan@elementaryrobotics.com",
+    license="Apache License",
+    install_requires=[
+        "Cython==0.29.16",
+        "hiredis==1.1.0",
+        "msgpack==0.6.2",
+        "numpy==1.18.3",
+        "prompt-toolkit==2.0.7",
+        "psutil==5.8.0",
+        "pyarrow==0.17.0",
+        "pyfiglet==0.7.6",
+        "redis==3.5.3",
+        "redistimeseries==1.4.3",
+        "rmtest==0.7.0",
+        "six==1.15.0",
+        "wcwidth==0.2.5",
+        "typing_extensions==3.7.4.3",
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+    ],
 )
